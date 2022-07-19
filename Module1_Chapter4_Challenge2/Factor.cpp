4ce2731fb94fa43ce7983865b77afabc865642fc
SamYoungABK <109538468+SamYoungABK@users.noreply.github.com>
2022-07-19 13:41:53 -0700

    Added Factoring challenge

---
+++ Module1_Chapter4_Challenge2/Factor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+	int numberToFactor;
+
+	std::cout << "Enter a number to factor: " << std::endl;
+	std::cin >> numberToFactor;
+
+
+	std::cout << "Factors: ";
+	for (int i = 1; i < numberToFactor; i++) {
+		if (numberToFactor % i == 0) {
+			std::cout << i << ' ';
+		}
+	}
+}
