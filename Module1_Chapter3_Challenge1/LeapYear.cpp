76fd082ec3c967aeae6b55b1d28f12b4b35c873c
SamYoungABK <109538468+SamYoungABK@users.noreply.github.com>
2022-07-20 12:02:03 -0700

    removed overengineering

--- Module1_Chapter3_Challenge1/LeapYear.cpp
+++ Module1_Chapter3_Challenge1/LeapYear.cpp
@@ -5,15 +5,9 @@ bool isLeapYear(int year);
 
 int main()
 {
-	time_t now;
-	time(&now);
-	struct tm timeinfo;
-	localtime_s(&timeinfo, &now);
+	int year = 2022;
 
-	int year = 1900 + timeinfo.tm_year;
-
-	std::cout << "Enter the year to check if it's a leap year (Default: " <<
-		year << "): " << std::endl;
+	std::cout << "Enter a year to check if it's a leap year: " << std::endl;
 	std::cin >> year;
 
 	if (isLeapYear(year))
