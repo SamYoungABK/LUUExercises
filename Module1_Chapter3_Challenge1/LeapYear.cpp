b06977beb28fec07a40bf98f590e463b7e227e91
SamYoungABK <109538468+SamYoungABK@users.noreply.github.com>
2022-07-26 09:40:31 -0700

    Capitalized IsLeapYear

--- Module1_Chapter3_Challenge1/LeapYear.cpp
+++ Module1_Chapter3_Challenge1/LeapYear.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <ctime>
 
-bool isLeapYear(int year);
+bool IsLeapYear(int year);
 
 int main()
 {
@@ -10,20 +10,17 @@ int main()
 	std::cout << "Enter a year to check if it's a leap year: " << std::endl;
 	std::cin >> year;
 
-	if (isLeapYear(year))
-	{
+	if (IsLeapYear(year))
 		std::cout << year << " is a leap year!" << std::endl;
-	}
 	else
-	{
 		std::cout << year << " is not a leap year!" << std::endl;
-	};
 }
 
-bool isLeapYear(int year)
+bool IsLeapYear(int year)
 {
 	if (year % 400 == 0) return true;
 	if (year % 100 == 0) return false;
 	if (year % 4 == 0) return true;
+
 	return false;
 }
